c0e3c58c70c77e8789a49f3adbf4a8424335db05
fismapelajar <90302378+fismapelajar@users.noreply.github.com>
2022-10-06 17:00:12 +0700

    Add files via upload

---
+++ 2. Tugas Praktikum Relasi Antar Kelas/App.java
@@ -0,0 +1,14 @@
+/*  Fisma Meividianugraha Subani
+    21091397017*/
+    
+import RelasiAntarKelas.Limas;
+
+/**
+ * Class ini khusus untuk melakukan tes pada program
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        Limas limas = new Limas(1, 2, 5, 2, 2, 3, 2, 7);
+        limas.tampil();
+    }
+}
